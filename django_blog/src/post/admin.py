cf05ee7df111f6d1bdf060f635695dd0824741f6
makh.arman <a.makhanbet17@gmail.com>
2024-02-24 20:38:33 +0600

    second commit

--- django_blog/src/post/admin.py
+++ django_blog/src/post/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post) 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at', 'is_actual']
--- django_blog/src/post/models.py
+++ django_blog/src/post/models.py
@@ -1,5 +1,12 @@
+import random
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128, null=True, default=f'post {int(random.random() * 100)}')
     photo = models.ImageField('post/imgs/', null=True)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    is_actual = models.BooleanField(blank=True, null=True)
+    
+    
+    
